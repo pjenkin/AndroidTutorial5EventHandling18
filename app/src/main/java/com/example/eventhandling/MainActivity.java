7a4f657cffa3f734034ff7cc4528dab3e9c917fd
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-26 10:22:05 +0100

    22 Gesture recognizer (NB beware - 2 content_main.xml, 1 for v26)

--- app/src/main/java/com/example/eventhandling/MainActivity.java
+++ app/src/main/java/com/example/eventhandling/MainActivity.java
@@ -8,13 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText; // TextView?
-import android.view.View.OnLongClickListener;       // import this especially for OnlongClick to work?
+import android.view.MotionEvent;     // needed for gesture
+import android.view.GestureDetector;     // needed for gesture
+import android.support.v4.view.GestureDetectorCompat;     // needed for gesture
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
+// implement a couple of interfaces to handle gestures (OnGestureListener for several gesture types)
 
-public class MainActivity extends AppCompatActivity {
+    private EditText gestureText;                       // for use with a text control on this app
+    private GestureDetectorCompat gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        // gesture recogniser
+        this.gestureText = (EditText) findViewById(R.id.gesture_edit_text);
+        this.gestureDetector =  new GestureDetectorCompat(this, this);
+        gestureDetector.setOnDoubleTapListener(this);   // NB DoubleTap separate from other gestures (cf multiple inheritance, above)
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
 
 
         // Event handling: (1) add event listener (2) callback method.
@@ -69,6 +84,63 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // implementation for gesture recognise'ing
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        gestureText.setText(R.string.tap_confirmed_text);
+        return true;            // true indicating that the event was handled
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        gestureText.setText(R.string.double_tap_text);
+        return true;            // true indicating that the event was handled
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        gestureText.setText(R.string.double_tap_event_text);
+        return true;            // true indicating that the event was handled
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        gestureText.setText(R.string.down_text);
+        return true;            // true indicating that the event was handled
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        gestureText.setText(R.string.show_text);
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        gestureText.setText(R.string.up_text);
+        return true;            // true indicating that the event was handled
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        gestureText.setText(R.string.scroll_text);
+        return true;            // true indicating that the event was handled
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        gestureText.setText(R.string.long_press_text);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        gestureText.setText(R.string.fling_text);
+        return true;            // true indicating that the event was handled
+    }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -89,5 +161,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // end of gesture recognizers
+
+
+    // as well as gesture recognizer code, override onTouchEvent as well
+    // onTouch is the default event of Activity whenever user d'touch the screen
+
+    /**
+     * Called when a touch screen event was not handled by any of the views
+     * under it.  This is most useful to process touch events that happen
+     * outside of your window bounds, where there is no view to receive it.
+     *
+     * @param event The touch screen event being processed.
+     * @return Return true if you have consumed the event, false if you haven't.
+     * The default implementation always returns false.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // first, detect if event being processed touch was actually a gesture
+        this.gestureDetector.onTouchEvent(event);
+        // if not a gesture, go ahead to super-class as normal
+        return super.onTouchEvent(event);
     }
 }
